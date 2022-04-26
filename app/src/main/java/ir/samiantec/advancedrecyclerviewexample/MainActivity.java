963ee86c07c14df0e7723c3f2a982ac0198d82ec
Sina <Moradix96@gmail.com>
2022-04-26 10:20:21 +0430

    Attach adapter to MainActivity

--- app/src/main/java/ir/samiantec/advancedrecyclerviewexample/MainActivity.java
+++ app/src/main/java/ir/samiantec/advancedrecyclerviewexample/MainActivity.java
@@ -1,8 +1,16 @@
 package ir.samiantec.advancedrecyclerviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ir.samiantec.advancedrecyclerviewexample.Adapter.Adapter;
+import ir.samiantec.advancedrecyclerviewexample.Model.Section;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RecyclerView recyclerView = findViewById(R.id.rv);
+
+
+        final List<Section> sectionList = new ArrayList<>();
+
+        final List<String> list1= new ArrayList<>();
+        list1.add("L1 Item 1");
+        list1.add("L1 Item 2");
+        sectionList.add(new Section("Sec 1", list1));
+
+        final List<String> list2= new ArrayList<>();
+        list2.add("L2 Item 1");
+        list2.add("L2 Item 2");
+        list2.add("L2 Item 3");
+        sectionList.add(new Section("Sec 2", list2));
+
+        final List<String> list3= new ArrayList<>();
+        list3.add("L3 Item 1");
+        sectionList.add(new Section("Sec 3", list3));
+
+        final Adapter adapter = new Adapter(sectionList);
+
+        recyclerView.setAdapter(adapter);
     }
+
 }
