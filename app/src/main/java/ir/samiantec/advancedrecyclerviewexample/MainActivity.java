0fbb2ab559322a72223d5834ea7ae64685503ee2
Sina <Moradix96@gmail.com>
2022-04-28 19:36:20 +0430

    Cleanup MainActivity

--- app/src/main/java/ir/samiantec/advancedrecyclerviewexample/MainActivity.java
+++ app/src/main/java/ir/samiantec/advancedrecyclerviewexample/MainActivity.java
@@ -1,12 +1,11 @@
 package ir.samiantec.advancedrecyclerviewexample;
 
+import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,21 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
         final RecyclerView recyclerView = findViewById(R.id.rv);
 
-
         final List<Section> sectionList = new ArrayList<>();
 
-        final List<String> list1= new ArrayList<>();
+        final List<String> list1 = new ArrayList<>();
         list1.add("L1 Item 1");
         list1.add("L1 Item 2");
         sectionList.add(new Section("Sec 1", list1));
 
-        final List<String> list2= new ArrayList<>();
+        final List<String> list2 = new ArrayList<>();
         list2.add("L2 Item 1");
         list2.add("L2 Item 2");
         list2.add("L2 Item 3");
         sectionList.add(new Section("Sec 2", list2));
 
-        final List<String> list3= new ArrayList<>();
+        final List<String> list3 = new ArrayList<>();
         list3.add("L3 Item 1");
         sectionList.add(new Section("Sec 3", list3));
 
@@ -57,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
                 int position = viewHolder.getAdapterPosition();
                 try {
+                    //TODO Implement delete method
                     //arrayList.remove(position);
                     //adapter.notifyItemRemoved(position);
                 } catch (Exception e) {
